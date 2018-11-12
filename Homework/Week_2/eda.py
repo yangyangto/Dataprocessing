f047550a5702a1afd5db7245f20d5d7eedf61220
yangyangto <yangyang.yyt@gmail.com>
2018-11-12 16:18:36 +0100

    plots

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -3,6 +3,7 @@
 import csv
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 """
 This script does something, EDA
 """
@@ -20,39 +21,77 @@ def del_missings(data_frame):
     Remove the entire row when it consist a missing value/ 'unknown'.
     """
     data_frame = data_frame.replace('unknown', np.nan).dropna(how='any')
-    return data_frame
-
-def make_int(data_frame, old_colomn, colomn):
-    data_frame[colomn] = data_frame[colomn].apply(int)
-    data_frame[old_colomn] = data_frame[colomn]
+    # data_frame = data_frame[data_frame.Country != 'Suriname']
+    # data_frame.drop(data_frame.index[144])
     return data_frame
 
 def make_float(data_frame, colomn):
-    # data_frame[colomn] = data_frame[colomn].str.replace(',', '.').astype(float)
-    data_frame['Pop. Density (per sq. mi.)'] = data_frame['Pop. Density (per sq. mi.)'].str.replace(',', '.').astype(float)
+    """
+    Make values from a colomn type float.
+    """
+    data_frame[colomn] = data_frame[colomn].str.replace(',', '.').astype(float)
     return data_frame
 
 def preprocess_GDP(data_frame):
-    # remove 'dollars' from values in GDP colomn
-    # data_frame['GDP_colomn'] = data_frame['GDP ($ per capita) dollars'].str.strip('dollars')
-    # make_int(data_frame, 'GDP ($ per capita) dollars','GDP_colomn')
-    #
-    # return data_frame
+    """
+    Preprocess data for analyses
+    """
+    data_frame['Region'] = data_frame['Region'].str.rstrip(" ")
+    data_frame = make_float(data_frame, 'Pop. Density (per sq. mi.)')
+    data_frame = make_float(data_frame, 'Infant mortality (per 1000 births)')
 
-    data_frame['GDP ($ per capita) dollars'] = data_frame['GDP ($ per capita) dollars'].str.replace('dollars', '').astype(int)
+    GDP = data_frame['GDP ($ per capita) dollars']
+    GDP = GDP.str.strip('dollars').astype(int)
+
+    data_frame['GDP ($ per capita) dollars'] = GDP.mask(GDP > (GDP.mean() + GDP.std() * 3))
+    data_frame = del_missings(data_frame)
+    # print(data_frame)
+    # data_frame['GDP ($ per capita) dollars'] = data_frame['GDP ($ per capita) dollars'].str.replace('dollars', '').astype(int)
     return data_frame
+
+def plot_GDP(data_frame):
+    """
+    Plot a histogram with the GDP data.
+    """
+    n, bins, patches = plt.hist(data_frame['GDP ($ per capita) dollars'], bins='auto', color='lightseagreen',
+                            alpha=0.7, rwidth=0.85)
+    # plt.hist(data_frame['Region'], data_frame['GDP ($ per capita) dollars'])
+    # plt.axis([START_YEAR, END_YEAR - 1, 0, 10])
+    plt.title('GDP worldwide', fontsize=12)
+    plt.ylabel('Probability')
+    plt.xlabel('GDP ($ per capita) dollars')
+    plt.grid(True)
+    plt.show()
+
+def plot_infant_mort(data_frame):
+    """
+    Plot a boxplot with the Infant Mortality (per 1000 births).
+    """
+    # boxplot = data_frame.boxplot(column=['Infant mortality (per 1000 births)'])
+    df = pd.DataFrame(np.random.rand(10,5))
+    bp = df.boxplot()
+
+
+def CT(data_frame):
+        print(data_frame['GDP ($ per capita) dollars'].mean())
+        print(data_frame['GDP ($ per capita) dollars'].median())
+        print(data_frame['GDP ($ per capita) dollars'].mode())
+        print(data_frame['GDP ($ per capita) dollars'].std())
+        # print(df_values_only['GDP ($ per capita) dollars'].max())
 
 if __name__ == "__main__":
     INPUT_CSV = "input.csv"
     df = parse_data(INPUT_CSV)
     df_values_only = del_missings(df)
-
     df_values_only = preprocess_GDP(df_values_only)
-    df_values_only = make_float(df_values_only, 'Pop. Density (per sq. mi.)')
-    df_values_only = make_float(df_values_only, 'Infant mortality (per 1000 births)')
-    # df_values_preprocessed = make_int(df_values_only, "Population", 'Population')
-    print(df_values_only['Pop. Density (per sq. mi.)'])
-    print(df_values_only.iloc[1][2] * 2)
+
+ #Suriname 144
+
+    # print(df_values_only.iloc[144][0], df_values_only.iloc[144][8])
+    plot_GDP(df_values_only)
+
+
+
 
 
 
@@ -67,4 +106,9 @@ if __name__ == "__main__":
 #                 print("yes")
 #                 data_frame = data_frame.drop([i], axis=0)
 #                 break
+#     return data_frame
+
+# def make_int(data_frame, old_colomn, colomn):
+#     data_frame[colomn] = data_frame[colomn].apply(int)
+#     data_frame[old_colomn] = data_frame[colomn]
 #     return data_frame
