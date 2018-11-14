20cbf8e72650c5ef5dabf5c1824a6f70a23c5f83
yangyangto <yangyang.yyt@gmail.com>
2018-11-14 16:10:13 +0100

    final

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -9,21 +9,21 @@ import json
 This script does something, EDA
 """
 
-def parse_data(infile):
+def parse_data(infile, columns):
     """
-    Parse in data from input file.
+    Parse in (specific) data from input file.
     """
     data = pd.read_csv(infile)
-    df = pd.DataFrame(data)
-    df = df[['Country', 'Region', 'Pop. Density (per sq. mi.)',
-             'Infant mortality (per 1000 births)','GDP ($ per capita) dollars']]
-    return df
+    data_frame = pd.DataFrame(data)[columns]
+
+    return data_frame
 
 def del_missings(data_frame):
     """
     Remove the entire row when it consist a missing value/ 'unknown'.
     """
     data_frame = data_frame.replace('unknown', np.nan).dropna(how='any')
+
     return data_frame
 
 def make_float(data_frame, column):
@@ -38,7 +38,7 @@ def preprocess_data(data_frame):
     Preprocess data for analyses
     """
     # strip spaces on the right of all data in column Region
-    data_frame['Region'] = data_frame['Region'].str.rstrip(" ")
+    data_frame['Region'] = data_frame['Region'].str.rstrip()
 
     # make data in column Population density and Infant mortality type float
     data_frame = make_float(data_frame, 'Pop. Density (per sq. mi.)')
@@ -51,31 +51,36 @@ def preprocess_data(data_frame):
     return data_frame
 
 def exclude_outliers(data_frame, column):
+    """
+    Exclude the entire row if there's an outlier.
+    """
     df_column = data_frame[column]
-    data_frame[column] = df_column.mask(df_column > (df_column.mean() + df_column.std() * 3))
+    data_frame[column] = df_column.mask(df_column > (df_column.mean() +
+                                        df_column.std() * 3))
     data_frame = del_missings(data_frame)
 
     return data_frame
 
 def plot_histogram(data_frame, column, xlabel):
     """
-    Plot a histogram.
+    Plot a histogram using data from a specific column.
     """
     # create an histogram with the GDP data
     plt.hist(data_frame[column], bins='auto',
-             color='lightseagreen', alpha=0.7, rwidth=0.85)
+             alpha=0.7, rwidth=0.85)
 
     # define axes, titles etc.
     plt.title(column, fontsize=12)
     plt.ylabel('Frequency')
     plt.xlabel(xlabel)
     plt.grid(True)
+
     plt.show()
 
-def plot_infant_mort(data_frame, column, title):
+def plot_boxplot(data_frame, column, title):
     """
-    Plot one boxplot with the Total Infant Mortality (per 1000 births)
-    and one where it's grouped by Region.
+    Plot two boxplots, one using the all data altogether, one where it's
+    grouped by Region.
     """
     # create a boxplot with total infant mortality
     data_frame.boxplot(column=[column], grid=False)
@@ -84,74 +89,65 @@ def plot_infant_mort(data_frame, column, title):
     plt.ylabel(column)
 
     # create a boxplot with infant mortality per region
-    data_frame.boxplot(column=[column], by='Region', grid=False, rot=75)
+    data_frame.boxplot(column=[column], by='Region', grid=False, rot=78)
     name = title + ' per Region'
     plt.title(name)
     plt.ylabel(column)
+    plt.tight_layout()
 
-    # show boxplots with the infant mortality data
+    # remove the default (grouped by) title
+    plt.suptitle('')
+
     plt.show()
 
 def central_tendency(data_frame, column):
-        print('Central tendency of', column)
-        print('mean :   ', data_frame[column].mean())
-        print('median : ', data_frame[column].median())
-        print('mode :   ', data_frame[column].mode())
-        print('stdev. : ', data_frame[column].std(), '\n')
+    """
+    Print common measures of the Central Tendency.
+    """
+    print('Central tendency of', column)
+    print('mean :   ', data_frame[column].mean())
+    print('median : ', data_frame[column].median())
+    print('mode :   ', data_frame[column].mode()[0])
+    print('stdev. : ', data_frame[column].std(), '\n')
 
 def five_number(data_frame, column):
-        print('Count, Mean, std and the Five Number Summary of', column)
-        print(data_frame[column].describe())
+    """
+    Print the Count, Mean, std and the Five Number Summery.
+    """
+    print('Count, Mean, std and the Five Number Summary of', column)
+    print(data_frame[column].describe())
 
 def convert_json(data_frame):
-        # sets Country as index
-        data_frame = data_frame.set_index(['Country'])
-
-        # convert df to json format using the (new) index as key
-        outfile = data_frame.to_json(orient='index')
-
-        with open('eda.json', 'w') as f:
-            f.write(outfile)
+    """
+    Convert from pandas dataframe to json using 'Country' as index.
+    """
+    data_frame = data_frame.set_index(['Country'])
+    data_frame.to_json('eda.json', orient='index')
 
 if __name__ == "__main__":
+    # parse data from inputfile
     INPUT_CSV = "input.csv"
-    df = parse_data(INPUT_CSV)
+    df = parse_data(INPUT_CSV, ['Country', 'Region', 'Pop. Density (per sq. mi.)',
+                    'Infant mortality (per 1000 births)',
+                    'GDP ($ per capita) dollars'])
 
+    # clean and preprocess data
     df_values_only = del_missings(df)
     df_values_only = preprocess_data(df_values_only)
-    plot_histogram(df_values_only, 'GDP ($ per capita) dollars', 'GDP in dollars')
 
+    # plot two histograms using GDP data, one with and one without outliers
+    plt.style.use('seaborn-dark')
+    plot_histogram(df_values_only, 'GDP ($ per capita) dollars', 'GDP in dollars')
     df_values_only = exclude_outliers(df_values_only, 'GDP ($ per capita) dollars')
     plot_histogram(df_values_only, 'GDP ($ per capita) dollars', 'GDP in dollars')
 
+    # plot a boxplot using Infant mortality data
+    plot_boxplot(df_values_only, 'Infant mortality (per 1000 births)',
+                 'Infant Mortality')
+
+    # print central_tendency of GDP data and five number summary of infant mort.
     central_tendency(df_values_only, 'GDP ($ per capita) dollars')
     five_number(df_values_only, 'Infant mortality (per 1000 births)')
 
-    plot_infant_mort(df_values_only, 'Infant mortality (per 1000 births)', 'Infant Mortality')
-
+    # convert pandas dataframe to json file
     convert_json(df_values_only)
-
-     #Suriname 144
-
-
-# print(df_values_only.iloc[144][0], df_values_only.iloc[144][8])
-
-# data_frame['GDP ($ per capita) dollars'] = data_frame['GDP ($ per capita) dollars'].str.replace('dollars', '').astype(int)
-
-#
-# def del_missings(data_frame):
-#     """
-#     Remove the entire row when it consist a missing value.
-#     """
-#     for i in range(227):
-#         for j in range(20):
-#             if pd.isnull(data_frame.loc[i][j]): #not iloc, want je wilt niet indexen, maar label
-#                 print("yes")
-#                 data_frame = data_frame.drop([i], axis=0)
-#                 break
-#     return data_frame
-
-# def make_int(data_frame, old_column, column):
-#     data_frame[column] = data_frame[column].apply(int)
-#     data_frame[old_column] = data_frame[column]
-#     return data_frame
