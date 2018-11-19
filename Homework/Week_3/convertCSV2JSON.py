af34d1a4e0102e8181d1fc7ebc2c7d008f7414dc
yangyangto <yangyang.yyt@gmail.com>
2018-11-19 15:18:36 +0100

    week_3

---
+++ Homework/Week_3/convertCSV2JSON.py
@@ -0,0 +1,58 @@
+# Name: Yang Yang To
+# Student number:10340238
+
+import csv
+import pandas as pd
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+"""
+This script does something, CHANGE THIS!!!
+"""
+
+def parse_data(infile):
+    data = pd.read_csv(infile)
+    data_frame = pd.DataFrame(data)
+
+    return data_frame
+
+def convert_json(data_frame, title):
+    """
+    Convert from pandas dataframe to json using 'Country' as index.
+    """
+    data_frame = data_frame.set_index(['DATE'])
+    data_frame.to_json(title, orient='index')
+
+if __name__ == "__main__":
+    # parse data from inputfile
+    INPUT_CSV_men = "Employment_men.csv"
+    df_men = parse_data(INPUT_CSV_men)
+    print(df_men)
+
+    INPUT_CSV_women = "Employment_women.csv"
+    df_women = parse_data(INPUT_CSV_women)
+    print(df_women)
+
+    convert_json(df_men, 'employment_men.json')
+    convert_json(df_women, 'employment_women.json')
+
+
+    # df.boxplot(column='Value', by='LOCATION', rot=90)
+    # plt.show()
+    # print(df)
+
+
+#
+# def parse_old(infile):
+#     """
+#     Parse in and preprocess data from input file.
+#     """
+#     data = pd.read_csv(infile, parse_dates=['Datetime'], infer_datetime_format=True)
+#     data['Year'] = data.Datetime.dt.year
+#     data['Month'] = data.Datetime.dt.month
+#     data['Time'] = data.Datetime.dt.time
+#     data_frame = pd.DataFrame(data)
+#     data_frame['AEP_MW'] = data_frame['AEP_MW'].astype(int)
+#     data_frame = data_frame.loc[(data_frame['Year'] == 2004) | (data_frame['Year'] == 2018)]
+#     print(data_frame)
+#     return data_frame
