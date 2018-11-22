fba524ffde0b5ba505b6a561652d183b8bd9b699
yangyangto <yangyang.yyt@gmail.com>
2018-11-22 16:25:04 +0100

    final

--- Homework/Week_3/convertCSV2JSON.py
+++ Homework/Week_3/convertCSV2JSON.py
@@ -3,6 +3,7 @@
 
 import csv
 import pandas as pd
+import numpy as np
 import json
 
 """
@@ -17,6 +18,7 @@ def parse_data(infile1, infile2, column):
     second_data = pd.read_csv(infile2)
     data[column] = second_data[column]
     data_frame = pd.DataFrame(data)
+    data_frame = del_missings(data_frame)
 
     return data_frame
 
@@ -27,6 +29,14 @@ def convert_json(data_frame, title):
     data_frame = data_frame.set_index(['DATE'])
     data_frame.to_json(title, orient='index')
 
+def del_missings(data_frame):
+    """
+    Remove the entire row when it consist a missing value.
+    """
+    data_frame = data_frame.replace('.', np.nan).dropna(how='any')
+
+    return data_frame
+
 if __name__ == "__main__":
     # parse data from inputfiles
     INPUT_CSV_men = "Employment_men.csv"
@@ -34,4 +44,4 @@ if __name__ == "__main__":
     df = parse_data(INPUT_CSV_men, INPUT_CSV_women, 'LNS12300002')
 
     # convert into json format
-    convert_json(df, 'employment.json')
+    convert_json(df, 'employed.json')
